5757e95e14acda156b4c70f2469e2a85a06a6f70
sontixyou <kengoviolinphoto@gmail.com>
2025-06-14 10:10:03 +0900

    patch-feat: 🎸 add POST user endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,30 @@
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
 
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/libsql';
+import { usersTable } from './db/schema';
+
+const dbFileName = process.env.DB_FILE_NAME;
+if (!dbFileName) {
+  throw new Error('DB_FILE_NAME environment variable is not set.');
+}
+const db = drizzle(dbFileName);
 const app = new Hono();
 
 app.get('/', (c) => {
   return c.text('Hello Hono!');
+});
+
+app.post('/user', async (c) => {
+  insertUser()
+    .then(() => {
+      return c.json({ message: 'User created successfully!' });
+    })
+    .catch((error) => {
+      console.error('Error inserting user:', error);
+      return c.json({ error: 'Failed to create user' }, 500);
+    });
 });
 
 serve(
@@ -16,3 +36,15 @@ serve(
     console.log(`Server is running on http://localhost:${info.port}`);
   }
 );
+
+const insertUser = async () => {
+  const user: typeof usersTable.$inferInsert = {
+    name: 'John',
+    age: 30,
+    email: 'john@example.com',
+  };
+  await db.insert(usersTable).values(user);
+  console.log('New user created!');
+  const users = await db.select().from(usersTable);
+  console.log('Getting all users from the database: ', users);
+};
